916e85f4bcaa7a58dbc11760288ef1581055b2a0
Wes Hardee <weshardee@gmail.com>
2016-06-09 09:31:24 -0500

    update test cases to match newsletter

--- test.js
+++ test.js
@@ -2,6 +2,7 @@ import test from 'ava';
 import f from './index.js';
 
 test('f(1)', t => t.deepEqual(f(1), [2, 3, 4]));
-test('f(12)', t => t.deepEqual(f(12), [14, 15, 16]));
-test('f(25)', t => t.deepEqual(f(25), [26, 27, 28]));
-test('f(77)', t => t.deepEqual(f(77), [78, 78, 80]));
+test('f(4)', t => t.deepEqual(f(4), [6, 6, 8]));
+test('f(43)', t => t.deepEqual(f(43), [44, 45, 44]));
+test('f(123)', t => t.deepEqual(f(123), [124, 126, 124]));
+test('f(420)', t => t.deepEqual(f(420), [422, 423, 424]));
